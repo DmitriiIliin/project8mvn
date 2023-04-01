ffb34abf810129b605c48d2e0fa084bc9f4a385c
Dmitrii Iliin <dmitryilyin@mail.ru>
2023-04-01 21:18:05 +0300

    Initial commit

---
+++ src/main/java/ru/netology/project8/project8mvn/services/FormDate.java
@@ -0,0 +1,6 @@
+package ru.netology.project8.project8mvn.services;
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/ru/netology/project8/project8mvn/services/Post.java
@@ -0,0 +1,10 @@
+package ru.netology.project8.project8mvn.services;
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
